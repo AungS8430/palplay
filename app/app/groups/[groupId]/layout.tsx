9d071d6edf2ebd660699680ccb397a99cdd021c2
AungS8430 <aungshome@outlook.com>
2025-11-29 15:39:35 +0700

    Added route protection

--- app/app/groups/[groupId]/layout.tsx
+++ app/app/groups/[groupId]/layout.tsx
@@ -1,4 +1,7 @@
 import GroupTabs from "@/components/app/groups/groupTabs";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function GroupLayout({
   children,
@@ -8,6 +11,24 @@ export default async function GroupLayout({
   params: Promise<{ groupId: string }>;
 }) {
   const { groupId } = await params;
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return <div>Unauthorized</div>;
+  }
+
+  const userId = (session as any).userId as string;
+
+  const membership = await prisma.groupMember.findFirst({
+    where: {
+      groupId: groupId,
+      userId: userId,
+    },
+  });
+
+  if (!membership) {
+    return <div>Forbidden</div>;
+  }
   return (
     <div className="w-full">
       <GroupTabs groupId={groupId} />
