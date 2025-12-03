a51e8bec72c58db124b794eb52702f9be3c81d88
AungS8430 <aungshome@outlook.com>
2025-12-03 12:20:54 +0700

    Added option and reply buttons to messages

--- components/app/chat/chatItem.tsx
+++ components/app/chat/chatItem.tsx
@@ -3,10 +3,11 @@
 import { useState, useEffect } from "react";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Reply } from "lucide-react";
+import { Reply, EllipsisVertical } from "lucide-react";
 import SpotifyEmbed from "@/components/app/embeds/spotify";
 import YouTubeEmbed from "@/components/app/embeds/youtube";
 import ClientDateTime from "@/components/clientDateTime";
+import { Button } from "@/components/ui/button";
 
 interface MemberData {
   member: any;
@@ -32,17 +33,22 @@ export default function ChatItem({ groupId, out, text, replyToId, spotifyUri, yo
   }, [replyToId, groupId]);
 
   if (!memberDataProp) {
-    return <div>Loading...</div>;
+    return <></>;
   }
 
   return (
     <div className={"flex flex-row gap-2 items-end " + (out ? " justify-end" : " justify-start")}>
       {
-        !out && (
+        !out ? (
           <Avatar className="w-9 h-9">
             <AvatarImage src={memberDataProp.user.name} />
             <AvatarFallback>{memberDataProp.user.name.split(/[^A-Za-z]/)[0][0]}{(memberDataProp.user.name.split(/[^A-Za-z]/)?.length && memberDataProp.user.name.split(/[^A-Za-z]/)?.length || 0 > 1) && memberDataProp.user.name.split(/[^A-Za-z]/)[1][0]}</AvatarFallback>
           </Avatar>
+        ) : (
+          <div className="flex flex-row">
+            <Button size="icon-sm" variant="ghost" className="rounded-full"><EllipsisVertical className="text-neutral-400" /></Button>
+            <Button size="icon-sm" variant="ghost" className="rounded-full"><Reply className="text-neutral-400" /></Button>
+          </div>
         )
       }
       <div className="flex flex-col gap-1">
@@ -82,6 +88,14 @@ export default function ChatItem({ groupId, out, text, replyToId, spotifyUri, yo
           <p className={"text-xs font-semibold " + (out ? "text-neutral-600" : "text-neutral-400")}><ClientDateTime isoString={createdAt} /> { editedAt && (<span> · Edited At <ClientDateTime isoString={editedAt} /></span>)}</p>
         </div>
       </div>
+      {
+        !out && (
+          <div className="flex flex-row">
+            <Button size="icon-sm" variant="ghost" className="rounded-full"><Reply className="text-neutral-400" /></Button>
+            <Button size="icon-sm" variant="ghost" className="rounded-full"><EllipsisVertical className="text-neutral-400" /></Button>
+          </div>
+        )
+      }
     </div>
   )
 }
